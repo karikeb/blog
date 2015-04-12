07adb86ddd39d8c658ece7f8f7962ab380f8fdb2
karina <karina@ubuntu>
2015-04-12 09:02:47 -0300

    Creating and Updating Posts

--- protected/models/Post.php
+++ protected/models/Post.php
@@ -139,4 +139,35 @@ class Post extends CActiveRecord
                 'title'=>  $this->title,
             ));
         }
+        
+        protected function beforeSave()
+        {
+            if(parent::beforeSave())
+            {
+                if($this->isNewRecord)
+                {
+                    $this->create_time=$this->update_time=time();
+                    $this->author_id=Yii::app()->user->id;
+                }
+                else
+                    $this->update_time=time();
+                return true;
+            }
+            else
+                return false;
+        }
+        
+        protected function afterSave()
+        {
+            parent::afterSave();
+            Tag::model()->updateFrequency($this->_oldTags, $this->tags);
+        }
+        
+        private $_oldTags;
+        
+        protected function afterFind()
+        {
+            parent::afterFind();
+            $this->_oldTags=$this->tags;
+        }
 }
