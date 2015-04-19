40b36f2fabe836cdef495a15df42993176ee06e9
karina <karina@ubuntu>
2015-04-19 15:17:01 -0300

    Managing Comments

--- protected/controllers/CommentController.php
+++ protected/controllers/CommentController.php
@@ -122,7 +122,13 @@ class CommentController extends Controller
 	 */
 	public function actionIndex()
 	{
-		$dataProvider=new CActiveDataProvider('Comment');
+		$dataProvider=new CActiveDataProvider('Comment', array(
+                    'criteria'=>array(
+                        'with'=>'post',
+                        'order'=>'t.status, t.create_time DESC',
+                    ),
+                ));
+                
 		$this->render('index',array(
 			'dataProvider'=>$dataProvider,
 		));
@@ -170,4 +176,14 @@ class CommentController extends Controller
 			Yii::app()->end();
 		}
 	}
+        
+        public function actionApprove(){
+            if(Yii::app()->request->isPostRequest){
+                $comment=$this->loadModel();
+                $comment->approve();
+                $this->redirect(array('index'));
+            }
+            else
+                throw new CHttpException(400, 'Invalid request...');
+        }
 }
